fb7d19a256fd69958fbc30c2e7d3ce4dc575a988
Steve Giordano <steve1nyc@gmail.com>
2025-11-04 17:53:36 -0500

    Change api to be a proxy

--- api/api.py
+++ api/api.py
@@ -1,4 +1,4 @@
-from flask import Flask, jsonify
+from flask import Flask, request
 from dotenv import load_dotenv
 import os
 import requests
@@ -8,19 +8,20 @@ load_dotenv()
 app = Flask(__name__)
 
 IMMICH_API_KEY = os.getenv('IMMICH_API_KEY')
+
 @app.route("/images")
 def hello_world():
     return {'images': ['dogimage', 'catimage', 'rabbitimage']}
 
-@app.route('/tags')
-def get_tags():
-    try:
-        response = requests.get(f'http://147.93.180.221:2283/api/tags?apiKey={IMMICH_API_KEY}')
-        # Raises an HTTPError if the HTTP request returned an unsuccessful status code
-        response.raise_for_status()  
-        data = response.json()
-    except requests.exceptions.HTTPError as http_err:
-        return jsonify({'error': f'HTTP error occurred: {http_err}'}), 500
-    except Exception as err:
-        return jsonify({'error': f'Other error occurred: {err}'}), 500
-    return data
+
+@app.route('/<path:path>', methods=['GET', 'POST'])
+def proxy(path):
+    print('path', path)
+    if request.method == 'GET':
+        print('get')
+        return requests.get((f'http://147.93.180.221:2283/api/{path}?apiKey={IMMICH_API_KEY}')).content
+    
+    if request.method == 'POST':
+        print('post')
+        return requests.post((f'http://147.93.180.221:2283/api/{path}?apiKey={IMMICH_API_KEY}')).content
+    
